9f2f674ca6452394186e8ec0df0c9158b502e8fe
hritthik-insight <hritthik.bose@insight.com>
2024-01-14 20:42:09 +0530

    history 365 woking

--- src/components/History.js
+++ src/components/History.js
@@ -13,55 +13,71 @@ export default class History extends Component {
       item => item.id === parseInt(this.props.match.params.id)
     )[0];
 
-    let daysRecorded = habit.days;
+    let daysRecorded30 = habit.days;
     let dayGrids = [];
     let dayGridinYear = [];
+    let daysRecorded365 = [...daysRecorded30];
+    let taskCount = daysRecorded365.length;
 
 
     for (let i = 0; i < 30; i++) {
       let dayGridDate = moment()
         .subtract(i, "days")
         .format("YYYY-MM-D");
-      if (dayGridDate.toString() === daysRecorded[daysRecorded.length - 1]) {
+      if (dayGridDate.toString() === daysRecorded30[daysRecorded30.length - 1]) {
         dayGrids.unshift(<div className="day day--active" key={i} text={i}/>);
-        daysRecorded.splice(daysRecorded.length - 1, 1);
+        daysRecorded30.splice(daysRecorded30.length - 1, 1);
       } else {
         dayGrids.unshift(<div className="day" key={i} />);
       }
     }
 
+
+
     for (let i = 0; i < 365; i++) {
       let dayGridDate= moment()
         .subtract(i, "days")
         .format("YYYY-MM-D");
-      if (dayGridDate.toString() === daysRecorded[daysRecorded.length - 1]) {
+      if (dayGridDate.toString() === daysRecorded365[daysRecorded365.length - 1]) {
         dayGridinYear.unshift(<div className="day day--active" key={i} text={i}/>);
-        daysRecorded.splice(daysRecorded.length - 1, 1);
+        daysRecorded365.splice(daysRecorded365.length - 1, 1);
       } else {
         dayGridinYear.unshift(<div className="day" key={i} />);
       }
     }
+    
+    
+      
+    
 
     
     return (
       <div className="history">
-        <div className="history__details"> Total task statistics </div>
-        <div className="history__details"> Total task statistics </div>
         
-
-
         <div className="history__header">
-          <div>
+          <div className="task_name">
             <div className="habit__name habit__name--large">{habit.name}</div>
-            <div className="history__duration">Last 30 Days</div>
+        
           </div>
           <span className="history__close">
             <Link to="/" className="button button--danger">
               Back
             </Link>
           </span>
+
+        </div>
+        
+
+      
+        <p className="history__duration">Number of times You have done this task:         
+            <b className="task_count_summary">{taskCount}</b>
+        </p>
+
+        <div className="history__header">
+          <div className="history__duration">Last 30 Days</div>
         </div>
         <div className="days">{dayGrids}</div>
+        
         <div className="history__header">
           <div className="history__duration">Last 365 Days</div>
         </div>
